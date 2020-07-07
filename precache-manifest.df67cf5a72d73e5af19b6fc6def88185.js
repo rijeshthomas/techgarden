f748a3cee1063f3fb4eee2d3cc2abb8089647dc5
rijesh thomas <rijeshthomas@rijeshs-MBP.domain.name>
2020-07-06 20:10:20 -0700

    Updates

---
+++ precache-manifest.df67cf5a72d73e5af19b6fc6def88185.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb547a7dc5363ba9a4d9fe1f3b1139a",
+    "url": "/techgarden/index.html"
+  },
+  {
+    "revision": "f8d3db00ff51624f86b0",
+    "url": "/techgarden/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e03f84788b061178539a",
+    "url": "/techgarden/static/js/2.d58d5db8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/techgarden/static/js/2.d58d5db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d3db00ff51624f86b0",
+    "url": "/techgarden/static/js/main.3d5be15d.chunk.js"
+  },
+  {
+    "revision": "f4a9c01df3a23ec18986",
+    "url": "/techgarden/static/js/runtime-main.0267604e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/techgarden/static/media/logo.5d5d9eef.svg"
+  }
+]);
